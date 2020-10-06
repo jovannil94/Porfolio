fde7333fb9bb2c3aa2cb6b05434e4959e6463648
jovannil94 <jovanniluna@pursuit.org>
2020-10-06 14:34:16 -0400

    more animations details

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
   return (
     <div className="App">
       <About/>
+      <EducationSkills/>
       {/* <NavBar/> */}
       {/* <Switch>
         <Route exact path={"/"} component={About}/>
