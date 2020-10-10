cc365f1f71b167b75e7bb722b1a48ec8c0378766
jovannil94 <jovanniluna@pursuit.org>
2020-10-10 13:59:01 -0400

    fixes resume button css

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -11,9 +11,8 @@ export const Contact = ({ contactSection }) => {
                 <div className="contactLeft">
                     <h1 className="contactFormTitle">More Info</h1>
                     <div className="contactButtonContainer">
-                        <a href={pdf} without rel="noopener noreferrer" target="_blank">
-                            resume
-                            {/* <button className="contactResumeButton" trailingIcon="picture_as_pdf" label="Resume"><span><i class="contactResume fas fa-file-alt"/></span> Resume</button>    */}
+                        <a className="contactResumePdf" href={pdf} without rel="noopener noreferrer" target="_blank">
+                            <button className="contactResumeButton" trailingIcon="picture_as_pdf" label="Resume"><span><i class="contactResume fas fa-file-alt"/></span> Resume</button>   
                         </a>
                         <button className="contactGitButton" onClick={()=>window.open('https://github.com/jovannil94')}><span><i class="contactGit fab fa-github-square"/> jovannil94</span></button>
                         <button className="contactLinkButton" onClick={()=>window.open('https://www.linkedin.com/in/jovanniluna')}><span><i class="contactLink fab fa-linkedin"/></span> jovanniluna</button>
